2b079f232dce3c8eee7f6fab8f5a5f08761e4bbe
Herobrine25mcpe <imherobrine25@gmail.com>
2022-03-14 01:46:01 +0530

    first layout

--- test.py
+++ test.py
@@ -4,19 +4,24 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.image import Image
+from kivy.uix.floatlayout import FloatLayout
 
 
 # this is the main grid
 
+Builder.load_file('test.kv')
 
-class MyGridLayout(Widget):
-    def press(self):
-        name = self.name.text
+class MainLayout(Widget):
+    pass
 
 
 class Testapp(App):
     def build(self):
-        return MyGridLayout()
+        Window.clearcolor = (9/255, 24/255, 77/255, 1)
+        return MainLayout()
 
 
 if __name__ == "__main__":
