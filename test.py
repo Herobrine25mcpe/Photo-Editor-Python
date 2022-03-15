3cd43ae7bef053f5a88be6fb675d1dd4e7bd6495
Herobrine25mcpe <imherobrine25@gmail.com>
2022-03-16 01:03:55 +0530

    apply, preview, download

--- test.py
+++ test.py
@@ -1,36 +1,61 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.image import Image
-from kivy.uix.floatlayout import FloatLayout
 
 
 # this is the main grid
 
 Builder.load_file('test_kv.kv')
 
-
-class MainLayout(Widget):
-    def selected(self, filename):
-        try:
-            self.ids.image1.source = filename[0]
-
-        except:
-            pass
+global fimage
+fmagex = "images/img1.jpg"
 
 
 class FullImage(Image):
     pass
 
 
+bgcolor = (0.54, 0.54, 0.54, 1)
+
+
+class MainLayout(Widget):
+    # Image Selector Function
+    def selected(self, filename):
+
+        try:
+            self.ids.image1.source = filename[0]
+        except:
+            pass
+
+    def apply(self):
+        try:
+            self.ids.image2.source = fmagex
+        except:
+            pass
+
+    def preview(self):
+        try:
+            self.ids.image2.source = fmagex
+        except:
+            pass
+
+    def download(self):
+        try:
+            self.ids.image2.source = fmagex
+        except:
+            pass
+
+
+
+
+
 class Testapp(App):
     def build(self):
-        Window.clearcolor = (0.54, 0.54, 0.54, 1)
+        Window.clearcolor = bgcolor
         return MainLayout()
 
 
